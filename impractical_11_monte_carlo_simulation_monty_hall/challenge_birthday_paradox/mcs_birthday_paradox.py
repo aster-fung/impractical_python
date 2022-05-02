d4538fc1c108eb7fe56c583bbb9c7707f5a0e028
aster <asterfung24@gmail.com>
2022-05-02 16:49:06 +0800

    solved bday problem with MCS

--- impractical_11_monte_carlo_simulation_monty_hall/challenge_birthday_paradox/mcs_birthday_paradox.py
+++ impractical_11_monte_carlo_simulation_monty_hall/challenge_birthday_paradox/mcs_birthday_paradox.py
@@ -8,9 +8,7 @@ output:
 """
 import datetime
 from random import randrange
-from collections import Counter
-import numpy as np
-
+import pandas as pd
 
 
 def generate_bdays(population):
@@ -86,75 +84,126 @@ def zero_cleanup(total, match):
             del match[i]
         i +=1
     check = len(total) == len(match)
-    print("lenisequal", check)
-    print ('0 in total: ', 0 in total)
-    print('0 in match: ', 0 in match)
-    print('------')
+    #print("lenisequal", check)
+    #print ('0 in total: ', 0 in total)
+    #print('0 in match: ', 0 in match)
     return total, match
 
-"""
-def generate_probi_list(total_list, match_list):
-    
-    param: 
-    total_list: 1D list: no of ppl in the room 
-    match_list: 1D list: no of ppl with matching bdays corresponding to total 
-    
-    prob_list = []
-    for i in range(1, len(total_list)):
-        # try:
-        print('match: ', match_list[i], 'total: ', total_list[i])
-        prob = match_list[i]/total_list[i]
-        prob_list.append(prob)
-        # except ZeroDivisionError: 
-        #    pass
-    print('probaility:')
-    print(prob_list[0:5])
-    return prob_list 
-"""
 
+def generate_probability(total, match):
+    """
+    param: 
+    total: 1D list: no of ppl in the room 
+    match: 1D list: no of ppl with matching bdays corresponding to total 
+    """
+    prob_list = []
+    for i in range(0, len(total)):
+        # print('total: ', total[i],'match: ', match[i])
+        prob = match[i]/total[i]
+        prob_list.append(prob)
+    return prob_list 
+
+
+def minimal_total(total, probi,target = 0.5):
+    """
+    calculates minimal total number of people needed to reach the target probabilty
+
+    param:
+    total: 1D list: no of ppl in the room per iter
+    probi: 1D list: corresponding probability to get ppl with identical bdays in the room per iter
+    target: threshold probability for minimal total ppl cutoff
+
+    returns:
+    minimal: int: round up from float; minimal no of ppl need to reach the target prob with matching bdays
+    """
+    current_probi = 0
+    current_total = 0
+    for i in range(0, len(total)):
+        if probi[i] >= target:
+            current_probi = probi[i]
+            current_total = total[i]
+            break
+    return current_total, current_probi
+
+
+def display_min (simulation_cycles, min_total, min_prob):
+    """
+    param:
+    simulation_cycles: user defined no of runs
+    min_total: a list of minimum no of ppl needed for threshold probability
+    min_prob: list of probability maching min_total
+
+    return:
+    df: Pandas.DataFrame object 
+    """
+    cycles = [cycle for cycle in range(1, simulation_cycles+1)]
+    zipped = list(zip(cycles, min_total, min_prob))
+    df = pd.DataFrame(zipped, columns = ['cycle', 'minimal people required', 'probability matching bdays'])
+    print(df)
+    return df
 
 def main():
+    play = True
+    while play:
+        check_input = False
+        while not check_input:
+            popul_range_str = input('maximum numbers of peoples in the room to test (2~ ): ')
+            simulation_cycles_str = input('no of simulation cycles (10~ ):')
+            try:
+                popul_range = int(popul_range_str)
+                simulation_cycles = int(simulation_cycles_str)      
+                # limit total iteration to 10000 times?
+                if popul_range>=2 or simulation_cycles>=10:
+                    check_input = True
+                    print('simulation running')
+                else:
+                    print('please enter the numbers in the range specified')
+            except ValueError:
+                print('please input integers only')
+                continue
+        
+        print('.',end = '')
+        total_turn = 0                       # total no of iteration for monitoring
+        min_total_collection = []
+        min_prob_collection = []
 
-    check_input = False
-    while not check_input:
-        popul_range_str = input('maximum numbers of peoples in the room to test (2~ ): ')
-        simulation_cycles_str = input('no of simulation cycles (10~ ):')
-        try:
-            popul_range = int(popul_range_str)
-            simulation_cycles = int(simulation_cycles_str)      
-            # limit total iteration to 10000 times?
-            if popul_range>=2 or simulation_cycles>=10:
-                check_input = True
-            else:
-                print('please enter the numbers in the range specified')
-        except ValueError:
-            print('please input integers only')
-            continue
-    
-    total_turn = 0                       # total no of iteration for monitoring
-    total_popul_per_iter = []            # 'key' to zip total no. of ppl in the room
-    match_popul_per_iter = []            # 'value' to zip no of ppl who have matches
-    
-    for iter in range(simulation_cycles):
-        probi_per_iter = []
-        for popul in range(0,popul_range+2,2):
-            total_turn += 1
-            # print('total turn: ', total_turn)
-            # print('iterate cycle: ', turn+1)
+        for iter in range(simulation_cycles):
+            #probi_per_iter = []                 # for clarity. in fact reinit in function 
+            total_popul_per_iter = []            # 
+            match_popul_per_iter = []            # 
             
-            bdays = generate_bdays(popul)                          
-            popul_match = count_bday_match(popul, bdays)           
-            total_popul_per_iter.append(popul)
-            match_popul_per_iter.append(popul_match)
-            #print('total ppl')
-            print(total_popul_per_iter)
-            #print(len(total_popul_per_iter))
-            # print('match')
-            print(match_popul_per_iter)
-            #print(len(match_popul_per_iter))
+            for popul in range(0,popul_range+2,2):
+                total_turn += 1       
+                bdays = generate_bdays(popul)                          
+                popul_match = count_bday_match(popul, bdays)           
+                total_popul_per_iter.append(popul)
+                match_popul_per_iter.append(popul_match)
+
+            total_popul_per_iter, match_popul_per_iter = zero_cleanup(total_popul_per_iter,match_popul_per_iter)   
+            probi_per_iter = generate_probability(total_popul_per_iter, match_popul_per_iter)
+            min_total, min_probi = minimal_total(total_popul_per_iter, probi_per_iter, 0.5)
+            if min_probi > 0.5:
+                min_total_collection.append(min_total)
+                min_prob_collection.append(min_probi)
+        
+        if len(min_total_collection) < 1:
+            print('not enough people please rerun with more people')
+            play = False
+            break
             
-        total_popul_per_iter, match_popul_per_iter = zero_cleanup(total_popul_per_iter,match_popul_per_iter)   
-        # probi_per_iter = generate_probi_list(total_popul_per_iter, match_popul_per_iter)
+
+        # present data table
+        print("=====================================================")
+        print("simulation result:")
+        display_min(iter, min_total_collection, min_prob_collection)
+        print("=====================================================")
+        
+        # calculate and present the average
+        average_min_total = round(sum(min_total_collection)/len(min_total_collection))
+        average_prob = round(sum(min_prob_collection)/ len(min_prob_collection), 2)
+        print ('The average minimal number people needs to reach 50% probability where half of the people share the birthday with at least one person in the room is ', average_min_total,' .')    
+        print ('The corresponding average probability is ', average_prob)
+
 
 if __name__ == '__main__':
     main()
